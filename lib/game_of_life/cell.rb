caa8fc1461496338cd52702b1444cfa93471ba77
bitfurry <sahilkharb@iitj.ac.in>
2015-11-16 11:11:32 +0530

    refactored Cell to produce a cell by reducing use of magic literals

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -1,5 +1,13 @@
 module GameOfLife
   class Cell
+    def initialize(state)
+      @state = state
+    end
+
+    def self.new_alive_cell
+      self.new('ALIVE')
+    end
+
     def is_alive?
       true
     end
--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 module GameOfLife
   describe Cell do
     context 'birth' do
-      let(:cell1) { Cell.new }
+      let(:cell1) { Cell.new_alive_cell }
 
-      it 'should produce a live cell' do
+      it 'should produce a alive cell' do
         expect(cell1.is_alive?).to eq(true)
       end
     end
