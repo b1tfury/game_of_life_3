974a69260a44ab4d990f6faf4800e30f5b055f75
bitfurry <sahilkharb@iitj.ac.in>
2015-11-16 11:53:56 +0530

    refactored cell specs to change the scope of instances

--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 module GameOfLife
   describe Cell do
-    context 'birth' do
-      let(:cell1) { Cell.new_alive_cell }
-      let(:cell2) { Cell.new_dead_cell }
+    let(:cell1) { Cell.new_alive_cell }
+    let(:cell2) { Cell.new_dead_cell }
 
+    
+    context 'birth' do
       it 'should produce a alive cell' do
         expect(cell1.is_alive?).to eq(true)
       end
