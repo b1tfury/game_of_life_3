b1d949242e8e46a22b2e672d65381d664afcb562
bitfurry <sahilkharb@iitj.ac.in>
2015-11-16 12:14:12 +0530

    refactred specs for homogeneity of quotes

--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -21,26 +21,26 @@ module GameOfLife
     end
 
     context 'birth' do
-      it 'should produce a alive cell' do
+      it "should produce a alive cell" do
         expect(cell1.is_alive?).to eq(true)
       end
 
-      it 'dead cell should not be alive' do
+      it "dead cell should not be alive" do
         expect(cell2.is_alive?).to eq(false)
       end
 
-      it 'should produce a dead cell also' do
+      it "should produce a dead cell also" do
         expect(cell2.is_dead?).to eq(true)
       end
 
-      it 'live cell should not be dead' do
+      it "live cell should not be dead" do
         expect(cell1.is_dead?).to eq(false)
       end
     end
 
-    context 'next generation ' do
+    context "next generation" do
       [0,1].each do |number|
-        it "alive cell should die if #{number} neighbours are`` alive" do
+        it "alive cell should die if #{number} neighbours are alive" do
           expect(cell1.next_generation(build_neighbours(number)).is_dead?).to eq(true)
         end
       end
